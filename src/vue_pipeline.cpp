1c5124e307e1b32df97e643bf059f6a246a6408e
xcus33me <HarvieOrigin@yandex.ru>
2024-04-22 22:21:27 +0300

    added bindingDesciptions and attributeDescriptions in VuePipeline::CreateGraphicsPipeline;

--- src/vue_pipeline.cpp
+++ src/vue_pipeline.cpp
@@ -1,5 +1,7 @@
 #include "vue_pipeline.hpp"
+#include "vue_model.hpp"
 
+// std
 #include <cassert>
 #include <fstream>
 #include <iostream>
@@ -77,13 +79,15 @@ namespace vue {
         shaderStages[1].flags = 0;
         shaderStages[1].pNext = nullptr;
         shaderStages[1].pSpecializationInfo = nullptr;
-
+        
+        auto bindingDescriptions = VueModel::Vertex::getBindingDescriptions();
+        auto attributeDescriptions = VueModel::Vertex::getAttributeDescriptions();
         VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
         vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-        vertexInputInfo.vertexAttributeDescriptionCount = 0;
-        vertexInputInfo.vertexBindingDescriptionCount = 0;
-        vertexInputInfo.pVertexAttributeDescriptions = nullptr;
-        vertexInputInfo.pVertexBindingDescriptions = nullptr;
+        vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+        vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(bindingDescriptions.size());
+        vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
+        vertexInputInfo.pVertexBindingDescriptions = bindingDescriptions.data();
 
         VkGraphicsPipelineCreateInfo pipelineInfo{};
         pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
